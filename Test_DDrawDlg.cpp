fa4e8de1811f5448624b935477c967e462f33efa
scpark98 <scpark98@gmail.com>
2025-09-27 00:48:20 +0900

    .

--- Test_DDrawDlg.cpp
+++ Test_DDrawDlg.cpp
@@ -124,16 +124,16 @@ BOOL CTestDDrawDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
-	m_filename = _T("D:\\홍매화.png");
+	m_filename = _T("D:\\ink-and-wash.png");
 	m_img.load(m_filename);
-	m_img.set_alpha(128);
+	//m_img.set_alpha(128);
 
 	CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
 	HRESULT hr = CreateDeviceIndependentResources();
 	hr = CreateDeviceResources();
-	//LoadBitmapFromFile(L"D:\\5120.jpg", &myBitmap);
-	LoadBitmapFromFile(L"D:\\권나라.jpg", &myBitmap);
+	LoadBitmapFromFile(L"D:\\다운로드.jfif", &myBitmap);
+	//LoadBitmapFromFile(L"D:\\권나라.jpg", &myBitmap);
 	LoadBitmapFromFile2(L"loading.gif", mySequenceBitmap.get());
 	LoadBitmapFromFile2(L"snail_small.png", myCharacterBitmap.get());
 
